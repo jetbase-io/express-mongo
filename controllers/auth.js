6815a40fe06092b5eea92ef5849940ad73b87dc4
Skelman50 <skelman50@gmail.com>
2020-08-11 16:00:16 +0300

    fixed iss8

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,17 +9,14 @@ exports.login = (req, res, next) => {
   passport.authenticate('login', async (err, user, message) => {
     try {
       if (err || !user) {
-        return res.status(401).json(message);
+        return res.status(400).json(message);
       }
       return req.login(user, { session: false }, async (error) => {
         if (error) return next(error);
         const body = { _id: user._id, email: user.email, role: user.role };
         const token = jwt.sign({ user: body }, process.env.JWT_SECRET, { expiresIn: '30m' });
         return res.json({
-          id: user.id,
-          token,
-          role: user.role,
-          permissions: permissions[user.role],
+          id: user.id, token, role: user.role, permissions: permissions[user.role],
         });
       });
     } catch (error) {
@@ -30,8 +27,9 @@ exports.login = (req, res, next) => {
 
 exports.logout = (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
-  Token.create({ value: token }).catch((err) => {
-    logger.error(err);
-  });
+  Token.create({ value: token })
+    .catch((err) => {
+      logger.error(err);
+    });
   res.status(200).send(errors('Logout successful'));
 };
